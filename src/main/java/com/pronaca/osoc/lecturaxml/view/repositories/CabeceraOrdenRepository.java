9c2198e394acd3f4928b9023f692636e452641fc
Lenin Ulcuango <lenin.ulcuango@bayteq.com>
2022-08-10 23:21:56 -0500

    Cambios CabeceraOrdenRepository findByParameters

--- src/main/java/com/pronaca/osoc/lecturaxml/view/repositories/CabeceraOrdenRepository.java
+++ src/main/java/com/pronaca/osoc/lecturaxml/view/repositories/CabeceraOrdenRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.pronaca.osoc.lecturaxml.model.entities.CabeceraOrden;
 import com.pronaca.osoc.lecturaxml.model.enums.EstadoEnum;
@@ -12,9 +13,21 @@ public interface CabeceraOrdenRepository extends JpaRepository<CabeceraOrden, Lo
 
 	List<CabeceraOrden> findByNumeroOrden(String numeroOrden);
 	
-	@Query("select c from CabeceraOrden c where (to_date(c.fechaProceso,'dd/MM/yyyy') between to_date(:fechaInicio,'dd/MM/yyyy') and to_date(:fechaFin,'dd/MM/yyyy')) and ((UPPER(c.numeroOrden) like conc(concat('%',UPPER(:orden)),'%')) or :orden='') and ((UPPER(c.proveedor) like concat(concat('%',UPPER(:codProveedor)),'%')) or :codProveedor='') and ((UPPER(c.identPropietario) like concat(concat('%',UPPER(:rucProveedor)),'%')) or :rucProveedor='') and ((UPPER(c.compania) like concat(concat('%',UPPER(:codEmpresa)),'%')) or :codEmpresa='') and (c.estadoRevision=:estado or :estado='TODOS') ")
-	List<CabeceraOrden> findByParametros(String fechaInicio, String fechaFin, String orden, String codProveedor, String rucProveedor, String codEmpresa, EstadoEnum estado);
+	@Query("select c from CabeceraOrden c where (to_date(c.fechaProceso,'dd/MM/yyyy') between to_date(:fechaInicio,'dd/MM/yyyy') and to_date(:fechaFin,'dd/MM/yyyy'))"
+			+ " and ((UPPER(c.numeroOrden) like concat(concat('%',UPPER(:orden)),'%')) or :orden='')"
+			+ " and ((UPPER(c.proveedor) like concat(concat('%',UPPER(:codProveedor)),'%')) or :codProveedor='')"
+			+ " and ((UPPER(c.identPropietario) like concat(concat('%',UPPER(:rucProveedor)),'%')) or (c.identPropietario is null) or :rucProveedor='')"
+			+ " and ((UPPER(c.compania) like concat(concat('%',UPPER(:codEmpresa)),'%')) or :codEmpresa='')"
+			+ " and (c.estadoRevision=:estado or :estado='TODOS') ")
+	List<CabeceraOrden> findByParametros(@Param(value = "fechaInicio") String fechaInicio, 
+			@Param(value = "fechaFin") String fechaFin, 
+			@Param(value = "orden") String orden, 
+			@Param(value = "codProveedor") String codProveedor, 
+			@Param(value = "rucProveedor") String rucProveedor, 
+			@Param(value = "codEmpresa") String codEmpresa, 
+			@Param(value = "estado") EstadoEnum estado);
 	
 	@Query("select c from CabeceraOrden c ")
 	List<CabeceraOrden> findAll();
+	
 }
